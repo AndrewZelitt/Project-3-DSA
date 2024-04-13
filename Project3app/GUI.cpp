e1aab68ecb2b99d73af82af6a520ac5915403cb9
AZelitt <azelitt@ufl.edu>
2024-04-13 01:48:40 -0400

    Visual studio does not want to show me properties so i cannot add the fltk directory into the program.

--- Project3app/GUI.cpp
+++ Project3app/GUI.cpp
@@ -1,14 +1,12 @@
 //this will be the gui cpp file for our project
 //written by Andrew Zelitt
 #include "GUI.h"
-#include <FL/platform.H>
-#include <iostream>
-#include "B+tree.cpp"
-#include "Red-Black-Tree.cpp"
+
 
 using namespace std;
 
-main() {
+int main() {
 
 	cout << "hello world";
+	return 0;
 }
--- Project3app/GUI.h
+++ Project3app/GUI.h
@@ -1,4 +1,12 @@
 
 #pragma once
 
+#include "GUI.h"
 #include <iostream>
+#include <vector>
+#include <FL/FL.H>
+
+
+
+#include "B+tree.cpp"
+#include "Red-Black-Tree.cpp"
